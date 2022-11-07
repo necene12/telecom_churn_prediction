525301b04aff224dacea1e3d55845e566f04603c
necene12 <nziechris@gmail.com>
2022-11-08 00:50:34 +0100

    mise à jour api v6

--- Apps/api_fastapi.py
+++ Apps/api_fastapi.py
@@ -1,10 +1,8 @@
 #Import des packages
-from os import sep
 import pandas as pd
 import numpy as np
-from fastapi import Depends, FastAPI
-from typing import List, Optional
-from pydantic import BaseModel
+from fastapi import FastAPI,File, UploadFile
+from pydantic import BaseModel, parse_obj_as
 import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
@@ -14,56 +12,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import AdaBoostClassifier
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline
-import joblib
+from fastapi.responses import HTMLResponse
+from typing import List
+from io import BytesIO
 
-api = FastAPI(
+api=FastAPI(
     title="API pour l'exécution du modèle prédictif",
     description="Sélection d'un modèle et obtention des prédictions à travers FastAPI",
-    version="2.0"
+    version="3.0"
 )
 
 # Route de bienvenu
-@api.get('/',name='Bienvenue')
+@api.get('/message/',name='Bienvenue')
 def get_index():
     """Affichage du message de bienvenue
     """
-    return {'message':'Cette API vous permettra d\'obtenir des prédictions de churn à partir des données d\'entrée de manière unitaire ou en masse par un fichier csv'}
+    return {'message':'Cette API vous permettra d\'obtenir des prédictions de churn à partir des données d\'entrée ou à partir d\'un csv'}
 
-# route de statut de l'api
-@api.get("/status", response_model=int, description="Returns 1 if API is healthy.")
-async def status():
-    return 1
-
-### Préparation des colonnes pour le pipeline d'encodage et de prédiction
-
-# Colonnes à supprimer suite au l'étape de préparation des données
-colonnes_a_supprimer=['customerID']
-# colonnes catégorielles
-colonnes_categorielles = ['gender',
-                          'SeniorCitizen',
-                          'Partner',
-                          'Dependents',
-                          'PhoneService',
-                          'MultipleLines',
-                          'OnlineSecurity',
-                          'Contract',
-                          'PaperlessBilling',
-                          'PaymentMethod',
-                          'StreamingMovies',
-                          'StreamingTV',
-                          'TechSupport',
-                          'DeviceProtection',
-                          'InternetService',
-                          'OnlineBackup']
-# Variables numériques
-colonnes_numeriques =['tenure',
-                      'MonthlyCharges',
-                      'TotalCharges']
-
-# Variable cible
-colonne_cible = ['Churn']
-
-# Typage des variables explicatives 
+# Typage des variables explicatives
 class VariablesExplicatives(BaseModel):
     """Classe pour définition de la liste des variables sur les quels se baseront le modèle prédictif
     """
@@ -87,106 +53,130 @@ class VariablesExplicatives(BaseModel):
     MonthlyCharges:float
     TotalCharges:float
 
-# Construction du pipelines de tranformation des données:
-numeric_transformer = Pipeline(steps=[
-       ('imputer', SimpleImputer(strategy='mean')),
-       ('scaler', StandardScaler())
-])
-
-categorical_transformer = Pipeline(steps=[
-       ('imputer', SimpleImputer(strategy='constant')),
-       ('onehot',OneHotEncoder())
-])
-
-preprocessor = ColumnTransformer(
-   transformers=[
-    ('numeric', numeric_transformer, colonnes_numeriques),
-    ('categorical', categorical_transformer, colonnes_categorielles)
-]) 
-
 ### Route de prédiction unitaires en fonctions des valeurs saisis par l'utilisateur
-@api.post('/prediction_unitaire', name='Retourne les prédictions à partir de valeurs de variables saisie manuellement par l\'utilisateur')
-def prediction_unitaire(variables_explicatives: VariablesExplicatives):
+@api.post('/prediction_unitaire/', name='Retourne la prédiction à partir de valeurs saisies manuellement par l\'utilisateur')
+def prediction_unitaire(var: VariablesExplicatives):
     """_summary_
     Args:
-        variables_explicatives (VariablesExplicatives): _description_
+        var (VariablesExplicatives): _description_
     Returns:
         _type_: _description_
     """
-    # Définition par l'utilisateur du modèle à considérer pour les prédictions
-    # Import des modèles entrainés:
-    # Import du modèle de régression logistique sauvegardé
-    reg_log = joblib.load(open('model_regressionlogistique.pkl','rb'))
-    # Import du modèle AdboostClassifier sauvegardé
-    adboostclass = joblib.load(open('model_AdboostClassifier.pkl', 'rb')) 
-    modele_choisi = input('Définir le modèle à utiliser = reg_log ou adboostclass?')
-    # Pipeline de prédiction sur la base des données saisie par l'utilisateur
-    pipeline_prediction = Pipeline(steps = [
-               ('preprocessor', preprocessor),
-               ('Oversampling', SMOTE()),
-               ('regressor',modele_choisi)
-           ])
-    return [pipeline_prediction.predict(variables_explicatives)]
+    loaded_model = pickle.load(open("model_regressionlogistique.pkl", "rb"))
+    valeurs =  [{
+        'gender' : var.gender,
+        'SeniorCitizen' : var.SeniorCitizen,
+        'Partner' : var.Partner,
+        'Dependents' : var.Dependents,
+        'PhoneService' : var.PhoneService,
+        'MultipleLines' : var.MultipleLines,
+        'OnlineSecurity' : var.OnlineSecurity,
+        'Contract' : var.Contract,
+        'PaperlessBilling' : var.PaperlessBilling,
+        'PaymentMethod' : var.PaymentMethod,
+        'StreamingMovies' : var.StreamingMovies,
+        'StreamingTV' : var.StreamingTV,
+        'TechSupport' : var.TechSupport,
+        'DeviceProtection' : var.DeviceProtection,
+        'InternetService' : var.InternetService,
+        'OnlineBackup' : var.OnlineBackup,
+        'tenure' : var.tenure,
+        'MonthlyCharges' : var.MonthlyCharges,
+        'TotalCharges' : var.TotalCharges
+    }]
 
+    df = pd.DataFrame(valeurs)
+    xpred=loaded_model.predict(df)
+    return xpred.tolist()[0]
+	
+### Route pour  import de fichier et prédiction en masse
+@api.post('/import_fichier_prediction/', name='Retourne les prédictions à partir du fichier csv sélectionné par l\'utilisateur')
+def upload(file: UploadFile):
+    """A partir de votre répertoire local,
+    sélectionner un fichier csv contant les données des 19 variables explicatives
+    pour la génération des prédictions"""
+    loaded_model = pickle.load(open("model_regressionlogistique.pkl", "rb"))
+    contents = file.file.read()
+    buffer = BytesIO(contents)
+    df = pd.read_csv(buffer, sep=';')
+    buffer.close()
+    file.file.close()
+    df_dict = df.to_dict(orient='records')
+    xpred = pd.DataFrame.from_records(df_dict)
+    pred = loaded_model.predict(xpred)
+    return pred.tolist()
 
-### Route de rédiction en masse à partir de fichier csv
-@api.post('/prediction_en_masse',name='Prédiction sur la base du modèle choisi et à partir des variables explicatives d\'un fichier csv')
-def prediction_en_masse():
-    """Affichage des prédictions sur la base des fichier csv contenant les variables explicatives
-    """
-    # construction du dataframe des variable cibles d'entrée
-    df = pd.read_csv(input('renseigner le nom du fichier suivi de l\'extention .csv du répertoire courant:'), sep=";")
-    # suppression des variables qu'on utilisera pas pour prédire la variables cible
-    X = df.drop(colonnes_a_supprimer, axis='columns')
-    # Import des modèles entrainés:
-    # Import du modèle de régression logistique sauvegardé
-    reg_log = joblib.load(open('model_regressionlogistique.pkl','rb'))
-    # Import du modèle AdboostClassifier sauvegardé
-    adboostclass = joblib.load(open('model_AdboostClassifier.pkl', 'rb'))
-    # Pipeline de prédiction sur la base des données saisie par l'utilisateur
-    modele_choisi = input('Définir le modèle à utiliser = reg_log ou adboostclass?') 
-    pipeline_prediction = Pipeline(steps = [
-               ('preprocessor', preprocessor),
-               ('Oversampling', SMOTE()),
-               ('regressor', modele_choisi)
-           ])
-    return [X.append(pipeline_prediction.predict(X))]
-    
 ### Route de réentrainement et sauvegrade du modèle
-
-@api.post('/reentrainement_du_modele',name='entrainement du modèle sur la base des données initiale pour assurer la bonne performance du modèle')
+@api.post('/reentrainement_du_modele/',name='Entrainement du modèle sur la base des données initiales pour mise à jour')
 def reentrainement():
     """entraine de sauvegarde nouveau le modèle
     """
-    # Chargement des données
-    dfsource = pd.read_csv("https://assets-datascientest.s3-eu-west-1.amazonaws.com/de/total/churn.csv")
-    dfsource = dfsource.reset_index()
-    dfsource = dfsource.replace(' ', np.nan)
-    dfsource['TotalCharges'] = dfsource['TotalCharges'].astype('float64')
-    dfsource.TotalCharges = dfsource.TotalCharges.fillna(0)
-    dfsource['SeniorCitizen'].replace({0:'No',1:'Yes'}, inplace=True)
-    dfsource['Churn'].replace({'No':0,'Yes':1}, inplace=True)
-    dfsource['TotalCharges'] = pd.to_numeric(dfsource['TotalCharges'])
-    dfsource = dfsource.drop(colonnes_a_supprimer,axis='columns')
-    # Pipeline de construction du modèle et sauvegarde
-    X = dfsource.drop(colonne_cible, axis='columns')
-    y = dfsource[colonne_cible]
-    # Transformation des données et construction du pipeline du modèle de regressionlogistique
+    df = pd.read_csv("https://assets-datascientest.s3-eu-west-1.amazonaws.com/de/total/churn.csv")
+    df = df.reset_index()
+    df = df.replace(' ', np.nan)
+    df['TotalCharges'] = df['TotalCharges'].astype('float64')
+    df.TotalCharges = df.TotalCharges.fillna(0)
+    df['SeniorCitizen'].replace({0:'No',1:'Yes'}, inplace=True)
+    df['Churn'].replace({'No':0,'Yes':1}, inplace=True)
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'])
+    colonnes_categorielles = ['gender',
+                          'SeniorCitizen',
+                          'Partner',
+                          'Dependents',
+                          'PhoneService',
+                          'MultipleLines',
+                          'OnlineSecurity',
+                          'Contract',
+                          'PaperlessBilling',
+                          'PaymentMethod',
+                          'StreamingMovies',
+                          'StreamingTV',
+                          'TechSupport',
+                          'DeviceProtection',
+                          'InternetService',
+                          'OnlineBackup']
+    colonnes_numeriques =['tenure',
+                          'MonthlyCharges',
+                          'TotalCharges']
+    colonnes_a_supprimer='customerID'
+    colonne_cible = 'Churn'
+    df = df.drop('index',axis='columns')
+    X = df.drop([colonne_cible, colonnes_a_supprimer], axis='columns')
+    y = df[colonne_cible]
+
+    numeric_transformer = Pipeline(steps=[
+           ('imputer', SimpleImputer(strategy='mean')),
+           ('scaler', StandardScaler())
+    ])
+
+    categorical_transformer = Pipeline(steps=[
+           ('imputer', SimpleImputer(strategy='constant')),
+           ('onehot',OneHotEncoder())
+    ])
+
+    preprocessor = ColumnTransformer(
+       transformers=[
+        ('numeric', numeric_transformer, colonnes_numeriques),
+        ('categorical', categorical_transformer, colonnes_categorielles)
+    ])
+
     pipeline_reglog = Pipeline(steps = [
-                ('preprocessor', preprocessor),
-                ('Oversampling', SMOTE()),
-                ('regressor',LogisticRegression())
-            ])
-    # Construction du modèle de regression logistique
+                   ('preprocessor', preprocessor),
+                   ('Oversampling', SMOTE()),
+                   ('regressor',LogisticRegression())
+               ])
     model_reglog = pipeline_reglog.fit(X, y)
+    # Sauvegarde du modèle de regression logistique
+    pickle.dump(model_reglog, open('model_regressionlogistique.pkl','wb'))
+
     # Pipeline adboost
     pipeline_adboost = Pipeline(steps = [
-                ('preprocessor', preprocessor),
-                ('Oversampling', SMOTE()),
-                ('regressor',AdaBoostClassifier())
-            ])
+                   ('preprocessor', preprocessor),
+                   ('Oversampling', SMOTE()),
+                   ('regressor',AdaBoostClassifier())
+               ])
     # Construction du modèle AdboostClassifier
     model_adboost = pipeline_adboost.fit(X,y)
-    joblib.dump(model_reglog, './model_regressionlogistique.pkl')
-    joblib.dump(model_adboost, './model_AdboostClassifier.pkl')
-    print("Les deux modèles Regression logistique et Classifieur Adboost ont été créés dans le même répertoire que celui de l'API")
+    # Sauvegarde du modèle AdboostClassifier
+    pickle.dump(model_adboost, open('model_AdboostClassifier.pkl', 'wb'))
+    return ("Modèles sauvegardés dans le répertoire back office de l\'API")
--- README.md
+++ README.md
@@ -1,39 +1,97 @@
-## Déploiement de modèles à travers FastApi, Docker et Kubernetes
+### Déploiement de modèles à travers FastApi, Docker et Kubernetes
+-----------------
 Ce répertoire contient le code et les différentes étapes relatives à : 
- - Le nettoyage et exploration des données.
- - La contruction, choix et sauvegarde des modèles.
- - La construction de l'API avec FastApi, contenant les routes: status, prediction unitaire, prédiction par fichier, réentrainement et sauvegarde du modèle.
- - La construction de l'image et du container Docker.
- - Au déploiement de l'API et du container sur Kubernetes.
- 
+ Le nettoyage et exploration des données.
+ La contruction, choix et sauvegarde des modèles.
+ La construction de l'API avec FastApi, contenant les routes: status, prediction unitaire, prédiction par fichier, réentrainement et  sauvegarde du modèle.
+ La construction de l'image et du container Docker.
+ Au déploiement de l'API et du container sur Kubernetes.
+-----------------
+
+#### Détail des fichiers: 
+-----------------
 Les fichiers relatifs aux différentes étapes sont les suivants: 
-- preprocessing.ipynb: Notebook qui permet de nettoyer et explorer les données.
-- construction_sauvegarde_de_modeles.ipynb : Pour la construction et sauvegarde des modèles à partir des données sources.
-- model_AdboostClassifier.pkl : fichier de sortie du modèle adboost classifier
-- model_regressionlogistique.pkl : fichier de sortie du modèle de regressions linéaire
-- setup.sh permet de construire l'image exécuter le container et sauvegarder l'image
-- donnees_test_predictions.csv : contient les données sources qui serviront à tester le modèle sur le point de terminaison de prédiction en masse de l'api
-- api_fastapi.py: le fichier de l'api
-- Dockerfile: qui contient les instruction pour la création et l'exécution du container docker
+```bash
+ Notebook qui permet de nettoyer et explorer les données
+ preprocessing.ipynb
+```
+```bash
+ Pour la construction et sauvegarde des modèles à partir des données sources
+ construction_sauvegarde_de_modeles.ipynb 
+```
+```bash
+ fichier de sortie du modèle adboost classifier
+ model_AdboostClassifier.pkl
+```
+```bash
+ fichier de sortie du modèle de regressions linéaire
+ model_regressionlogistique.pkl
+```
+```bash
+ permet de construire l\'image,exécute et sauvegarder le container
+ setup.sh 
+```
+```bash
+ contient les données sources qui serviront à tester le modèle sur le point de terminaison de prédiction en masse de l\'api
+ donnees_pour_test_route_predictions_en_masse.csv 
+```
+```bash
+ contient un dictionnaire de données qui serviront à tester le modèle sur le point de terminaison de prédiction unitaire de l\'api
+ donnees_pour_test_route_prediction_unitaire.txt 
+```
+```bash
+ le code de l\'api
+ api_fastapi.py 
+```
+```bash
+ contient les instruction pour la création et l\'exécution du container docker
+ Dockerfile
+```
+-----------------
 
+#### Installer les dépendances
+-----------------
 Installer les packages nécessaires au fonctionnement de l'api à travers le contenu du fichier requirements.txt
- pip3 install -r requirements.txt
+```bash
+$ pip3 install -r requirements.txt
+```
+-----------------
 
-Exécuté les commandes suivante à la racine du répertoire /Apps pour construire l'image docker, la sauvegarder et l'exécuter
-  cd /Apps
-  sh setup.sh
+#### Construire et sauvegarder le container Docker contenant l'API
+-----------------
+Exécuté les commandes suivante à la racine du répertoire où les fichiers ont été clonés pour construire l'image docker, la sauvegarder et l'exécuter
+```bash
+$  cd /Apps
+$  sh setup.sh
+  ```
+-----------------
 
-- Installer kubectl sur votre machine et exécuter les commandes suivantes:
+#### Exécuter l'API sous Kubernetes
+-----------------
+1. Installer kubectl sur votre machine et exécuter les commandes suivantes:
+```bash
   kubectl create -f churn_prediction_deployment.yml
-- déploiement de service de type clusterIP pour l'exposition de l'api
+```
+2. déploiement de service de type clusterIP pour l'exposition de l'api
+```bash
   kubectl create -f churn_prediction_service.yml
   kubectl expose deploy churn_prediction_deployment --type=ClusterIP --port=8002 --target-port=8000 --name churn_prediction_service
-- gestion de l'ingress pour l'exposition de l'api
+  ```
+3. gestion de l'ingress pour l'exposition de l'api
+```bash
   minikube addons enable ingress
   kubectl create -f churn_prediction_ingress.yml
-- obtenir l'adresse ip et le port de l'ingress par la commande suivante sur le quel l'api sera exposé
+```
+4. obtenir l'adresse ip et le port de l'ingress par la commande suivante sur le quel l'api sera exposé
+```bash
   kubectl get ingress
-- executer la commande suivante pour déployer l'api à l'adresse et le port de l'ingress
+```
+5. executer la commande suivante pour afficher l'interface graphique l'api à l'adresse et le port de l'ingress
+```bash
   ssh -i "nom du fichier contenant la clé ssh" ubuntu@IP_LOCALE_DE_LA_MACHINE -fNL 8000:IP_ingress
-- Chargez l'adress suivante sur votre navigateur pour essayer les points de terminaison de l'api directement sur l'interface OpenAPI de fastApi
+```
+6. Chargez l'adress suivante sur votre navigateur pour essayer les points de terminaison de l'api directement sur l'interface OpenAPI de fastApi
+```bash
   http://127.0.0.1:8000/docs
+  ```
+  -----------------
